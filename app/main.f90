a57befdc997a200f1c5c265caeca0ff34847c3fb
Dairen Jean <djean032@gmail.com>
2024-09-15 11:01:02 -0400

    refactored DLSODA calls to pure functions for potential concurrency

--- app/main.f90
+++ app/main.f90
@@ -3,73 +3,81 @@ program main
     use global
     use laser
     use chem
+    use stdlib_math, only: linspace
     implicit none
-    type(lsoda_class) :: ls_rates, ls_intensity
-    integer :: istate, itask, i, j, pos_idx
-    real(dp) :: z, zout, tout, rtol, atol(1), start, finish, y(5)
+    integer :: i, j, pos_idx
+    real(dp) :: z, z0, zout, t0, tout, start, finish
     real(dp), allocatable :: t(:)
     real(dp), allocatable :: intensities(:, :)
     real(dp), allocatable :: final_intensities(:, :)
-    real(dp) :: current_intensity(1)
+    real(dp), allocatable :: tot(:)
+    real(dp), allocatable :: int0(:)
     real(dp), allocatable :: z_pos(:)
     real(dp), allocatable :: z_sample(:)
     allocate(t(t_slices))
     allocate(z_sample(z_slices))
     allocate(intensities(z_pos_slices, t_slices))
     allocate(final_intensities(z_pos_slices, t_slices))
+    allocate(tot(z_pos_slices))
+    allocate(int0(z_pos_slices))
     allocate(z_pos(z_pos_slices))
-    allocate(pop(neq_rates, z_pos_slices))
+    allocate(pop(5, z_pos_slices))
 
 
-    rtol = 1.0e-8_dp
-    atol(1) = 1.0e-8_dp
-    itask = 1
-    istate = 1
-
-    ! Set the parameters
-    par(:) = [0._dp, frq, 3.2e-18_dp, 16.e-18_dp, 14.e-18_dp, &
-              1.0e-15_dp, 1.25e-9_dp, 1.0e-15_dp, 250.e-6_dp, 1.0e-15_dp]
 
     ! Set the initial conditions
     call cpu_time(start)
-    current_pop(:) = [1.75e18_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp]
-    t = linspace(-5.0e-8_dp, 5.0e-8_dp, t_slices)
-    z_pos = linspace(-20.0_dp, 20.0_dp, z_pos_slices)
+    ! Initialize y vectors for DLSODA
+    initial_pop(:) = [1.75e18_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp]
+    current_pop(:) = [1.75e18_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, &
+                      0._dp, frq, 3.2e-18_dp, 16.e-18_dp, 14.e-18_dp, &
+                      1.0e-15_dp, 1.25e-9_dp, 1.0e-15_dp, 250.e-6_dp, &
+                      1.0e-15_dp]
+    current_intensity(:) = [0._dp, 1.75e18_dp, 0.0_dp, 0.0_dp, 0.0_dp, &
+                            0.0_dp, frq, 3.2e-18_dp, 16.e-18_dp, 14.e-18_dp, &
+                            1.0e-15_dp, 1.25e-9_dp, 1.0e-15_dp, 250.e-6_dp, &
+                            1.0e-15_dp]
+    ! Initialize t and z values for DLSODA
+    t0 = 0.0_dp
+    tout = 1.0e-8_dp
+    z_pos = linspace(-30.0e-2_dp, 30.0e-2_dp, z_pos_slices)
     z_sample = linspace(0.0_dp, 1.0e-3_dp, z_slices)
-    do concurrent(j=1:z_slices)
-        pop(:, j) = current_pop
-    end do
+    t = linspace(-5.0e-8_dp, 5.0e-8_dp, t_slices)
+    z = 0.0_dp
+    zout = z_sample(2) - z_sample(1)
     do concurrent (j=1:z_pos_slices)
         z = z_pos(j)
         do concurrent(i=1:t_slices)
             intensities(j, i) = irradiance(0.0_dp, t(i), z)
         end do
     end do
-    do pos_idx=1, z_pos_slices
+    do concurrent (pos_idx=1:z_pos_slices)
+        do concurrent(j=1:z_slices)
+            pop(:, j) = initial_pop
+        end do
         do i=1, t_slices
-            current_intensity = intensities(pos_idx, i)
-            par(1) = intensities(pos_idx, i)
-            !print *, par(1)
+            current_intensity(1) = intensities(pos_idx, i)
             do j=1, z_slices
-                current_pop = pop(:, j)
-                call ls_rates%initialize(rhs_rates, neq_rates, istate=istate)
-                call ls_intensity%initialize(rhs_intensity, neq_intensity, istate=istate)
-                call ls_rates%integrate(current_pop, t(1), t(2), rtol, atol, itask, istate)
-                call ls_intensity%integrate(current_intensity, z_sample(1), z_sample(2), rtol, atol, itask, istate)
-                itask = 1
-                istate = 1
-                pop(:, j) = current_pop
+                 current_pop(1:5) = pop(:, j)
+                 current_pop(6) = current_intensity(1)
+                 current_pop = solve_rates(current_pop, t0, tout)
+                 current_intensity(2:6) = current_pop(1:5)
+                 pop(:, j) = current_pop(1:5)
+                 current_intensity = solve_intensity(current_intensity, z0, zout)
             end do
             final_intensities(pos_idx, i) = current_intensity(1)
         end do
     end do
     call cpu_time(finish)
+    tot = sum(final_intensities, dim=2)
+    int0 = sum(intensities, dim=2)
+    open(1, file="intensities.dat", status="unknown")
+    do i=1, z_pos_slices
+        write(1, *) z_pos(i), tot(i)/int0(i)
+    end do
     print *, "Time taken: ", finish - start
-!   do
-!      call ls%integrate(y, t, tout, rtol, atol, itask, istate)
-!      if (t .ge. t_fin) exit
-!      tout = tout + 10.0e-10_dp
-!   end do
 
 contains
 end program main
+
+
--- src/chem.f90
+++ src/chem.f90
@@ -1,52 +1,132 @@
 module chem
     use global
     implicit none
+    abstract interface
+        pure subroutine odepack_f(neq, t, y, ydot)
+          import :: dp
+          implicit none
+          integer, intent(in) :: neq
+          real(dp), intent(in) :: t
+          real(dp), intent(in) :: y(15)
+          real(dp), intent(out) :: ydot(neq)
+        end subroutine
+        
+        pure subroutine odepack_jac()
+        end subroutine
+    end interface
+    interface 
+        pure subroutine DLSODA(f, neq, y, t, tout, itol, rtol, atol, itask, &
+                          istate, iopt, rwork, lrw, iwork, liw, jac, jt)
+            import :: dp
+            implicit none
+            integer, intent(in) :: neq, itol, itask, iopt, lrw, liw, jt
+            integer, intent(inout) :: istate
+            real(dp), intent(in) :: rtol, atol(1)
+            real(dp), intent(inout) :: t
+            real(dp), intent(in) :: tout
+            real(dp), intent(inout) :: y(neq)
+            real(dp), intent(inout) :: rwork(lrw)
+            integer, intent(inout) :: iwork(liw)
+            procedure(odepack_f) :: f
+            procedure(odepack_jac) :: jac
+        end subroutine
+        
+    end interface 
 
     private
 
-    public :: rhs_rates, rhs_intensity
+    public :: rhs_rates, rhs_intensity, jdum, solve_rates, solve_intensity
 
 contains
-    subroutine rhs_rates(self, neq, t, y, ydot, ierr)
-        class(lsoda_class), intent(inout) :: self
+    pure subroutine rhs_rates(neq, t, y, ydot)
         integer, intent(in) :: neq
         real(dp), intent(in) :: t
-        real(dp), dimension(neq), intent(in) :: y
+        real(dp), dimension(15), intent(in) :: y
         real(dp), dimension(neq), intent(out) :: ydot
-        integer, intent(out) :: ierr
-        print *, par(1)
-        ydot(1) = -(par(3)*y(1)*par(1))/(h*par(2)) &
-                  + (y(2)/par(6)) &
-                  + (y(4)/par(9))
-        ydot(2) = (par(3)*y(1)*par(1))/(h*par(2)) &
-                  - (y(2)/par(6)) &
-                  - (par(4)*y(2)*par(1))/(h*par(2)) &
-                  + (y(3)/par(dp)) &
-                  - (y(4)/par(9))
-        ydot(3) = (par(4)*y(2)*par(1))/(h*par(2)) &
-                  - (y(3)/par(dp))
-        ydot(4) = -(par(5)*y(4)*par(1))/(h*par(2)) &
-                  + (y(5)/par(10)) &
-                  + (y(2)/par(7)) &
-                  - (y(4)/par(9))
-        ydot(5) = (par(5)*y(4)*par(1))/(h*par(2)) &
-                  - (y(5)/par(10))
-        ierr = 0
+        ydot(1) = -(y(8)*y(1)*y(6))/(h*y(7)) &
+                  + (y(2)/y(11)) &
+                  + (y(4)/y(14))
+        ydot(2) = (y(8)*y(1)*y(6))/(h*y(7)) &
+                  - (y(2)/y(11)) &
+                  - (y(9)*y(2)*y(6))/(h*y(7)) &
+                  + (y(3)/y(13)) &
+                  - (y(2)/y(12))
+        ydot(3) = (y(9)*y(2)*y(6))/(h*y(7)) &
+                  - (y(3)/y(13))
+        ydot(4) = -(y(10)*y(4)*y(6))/(h*y(7)) &
+                  + (y(5)/y(15)) &
+                  + (y(2)/y(12)) &
+                  - (y(4)/y(14))
+        ydot(5) = (y(10)*y(4)*y(6))/(h*y(7)) &
+                  - (y(5)/y(15))
     end subroutine rhs_rates
 
-    subroutine rhs_intensity(self, neq, t, y, ydot, ierr)
-        class(lsoda_class), intent(inout) :: self
+    pure subroutine rhs_intensity(neq, t, y, ydot)
         integer, intent(in) :: neq
         real(dp), intent(in) :: t
-        real(dp), dimension(neq), intent(in) :: y
+        real(dp), dimension(15), intent(in) :: y
         real(dp), dimension(neq), intent(out) :: ydot
-        integer, intent(out) :: ierr
-        ydot(1) = -par(3)*current_pop(1)*y(1) &
-                  - par(4)*current_pop(2)*y(1) &
-                  - par(5)*current_pop(4)*y(1)
-
-        ierr = 0
+        ydot(1) = -y(8)*y(2)*y(1) &
+                  - y(9)*y(3)*y(1) &
+                  - y(10)*y(5)*y(1)
     end subroutine rhs_intensity
+
+    pure subroutine jdum()
+    end subroutine jdum
+
+    pure function solve_rates(y, t, tout) result(y_ret)
+        integer :: neq, itol, itask, iopt, lrw, &
+                               liw, jt, istate
+        real(dp), intent(in) :: t, tout
+        real(dp) :: t_ret, tout_ret, rtol, atol(1), rwork(102)
+        real(dp), intent(in) :: y(15)
+        real(dp) :: y_ret(15)
+        integer :: iwork(25)
+        t_ret = t
+        tout_ret = tout
+        neq = 5
+        lrw = 102
+        liw = 25
+        rtol = 1.0e-8_dp
+        atol(1) = 1.0e-8_dp
+        itol = 1
+        itask = 1
+        istate = 1
+        iopt = 0
+        lrw = 102
+        liw = 25
+        jt = 2
+        y_ret = y
+        call DLSODA(rhs_rates, neq, y_ret, t_ret, tout_ret, itol, &
+        rtol, atol(1), itask, istate, iopt, rwork, lrw, iwork, liw, jdum, jt)
+    end function solve_rates
+
+    pure function solve_intensity(y, t, tout) result(y_ret)
+        integer :: neq, itol, itask, iopt, lrw, &
+                               liw, jt, istate
+        real(dp), intent(in) :: t, tout
+        real(dp) :: t_ret, tout_ret, rtol, atol(1), rwork(102)
+        real(dp), intent(in) :: y(15)
+        real(dp) :: y_ret(15)
+        integer :: iwork(25)
+        t_ret = t
+        tout_ret = tout
+        neq = 1
+        lrw = 102
+        liw = 25
+        rtol = 1.0e-8_dp
+        atol(1) = 1.0e-8_dp
+        itol = 1
+        itask = 1
+        istate = 1
+        iopt = 0
+        lrw = 102
+        liw = 25
+        jt = 2
+        y_ret = y
+        call DLSODA(rhs_intensity, neq, y_ret, t_ret, tout_ret, itol, &
+        rtol, atol(1), itask, istate, iopt, rwork, lrw, iwork, liw, jdum, jt)
+    end function solve_intensity
 
 end module chem
 
