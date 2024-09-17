252831c2c6bdbe942534792b341842ea525412fb
Dairen Jean <djean032@gmail.com>
2024-09-17 17:04:21 -0400

    starting min process

--- app/main.f90
+++ app/main.f90
@@ -3,6 +3,7 @@ program main
     use global
     use laser
     use chem
+    use stdlib_io, only: loadtxt
     use stdlib_math, only: linspace
     implicit none
     integer :: i, j, pos_idx
@@ -15,6 +16,10 @@ program main
     real(dp), allocatable :: normals(:)
     real(dp), allocatable :: z_pos(:)
     real(dp), allocatable :: z_sample(:)
+    real(dp), allocatable :: exp(:, :)
+    real(dp), allocatable :: z_exp(:)
+    real(dp), allocatable :: transmission(:)
+    integer :: skiprows = 10
     allocate(t(t_slices))
     allocate(z_sample(z_slices))
     allocate(intensities(z_pos_slices, t_slices))
@@ -26,22 +31,24 @@ program main
     allocate(pop(5, z_pos_slices))
 
 
+    call loadtxt('./data/6-Br/6-Br-pbt-532nm-154.2nJ.all', exp, skiprows=skiprows, fmt="*")
+    z_pos = exp(:, 1) * 1.0e-3_dp
+    transmission = exp(:, 3)/exp(:, 2)
 
     ! Set the initial conditions
     call cpu_time(start)
     ! Initialize y vectors for DLSODA
-    initial_pop(:) = [1.75e18_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp]
-    current_pop(:) = [1.75e18_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, &
-                      0.0_dp, frq, 3.1e-18_dp, 16.0e-18_dp, 15.0e-18_dp, &
-                      32.5e-9_dp, 0.3283e-9_dp, 1.0e-12_dp, 40000e-9_dp, &
+    initial_pop(:) = [2.90869e17_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp]
+    current_pop(:) = [2.90869e17_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, &
+                      0.0_dp, frq, 5.48e-18_dp, 16.0e-18_dp, 28.1e-18_dp, &
+                      1.0e-12_dp, 1.0e-12_dp, 1.0e-12_dp, 1.05e-7_dp, &
                       1.0e-12_dp]
-    current_intensity(:) = [0._dp, 1.75e18_dp, 0.0_dp, 0.0_dp, 0.0_dp, &
-                            0.0_dp, frq, 3.1e-18_dp, 16.0e-18_dp, 15.0e-18_dp, &
-                            32.5e-9_dp, 0.3283e-9_dp, 1.0e-12_dp, 40000e-9_dp, &
-                            1.0e-12_dp]
+    current_intensity(:) = [0.0_dp, 2.90869e17_dp, 0.0_dp, 0.0_dp, 0.0_dp, 0.0_dp, &
+                      frq, 5.48e-18_dp, 16.0e-18_dp, 28.1e-18_dp, &
+                      1.0e-12_dp, 1.0e-12_dp, 1.0e-12_dp, 1.05e-7_dp, &
+                      1.0e-12_dp]
     ! Initialize t and z values for DLSODA
     t0 = 0.0_dp
-    z_pos = linspace(-50.0e-3_dp, 50.0e-3_dp, z_pos_slices)
     z_sample = linspace(0.0_dp, 1.0e-1_dp, z_slices)
     t = linspace(-4.0e-9_dp, 4.0e-9_dp, t_slices)
     t0 = 0.0_dp
@@ -76,10 +83,13 @@ program main
     int0 = sum(intensities, dim=2)
     tot = sum(final_intensities, dim=2)
     normals = tot/int0
-    normals = normals + (1-maxval(normals))
+    normals = tot/int0 + (1.0_dp - maxval(normals))
+    transmission = transmission + (1.0_dp - maxval(transmission))
     open(1, file="intensities.dat", status="unknown")
+    open(2, file="exp.dat", status="unknown")
     do i=1, z_pos_slices
         write(1, *) z_pos(i), normals(i)
+        write(2, *) z_pos(i), transmission(i)
     end do
     print *, "Time taken: ", finish - start
 contains
